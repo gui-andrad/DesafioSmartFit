6fdafd9b923990c7527fc7cc4844670060c1d9a3
gui-andrad <devluizandrade@gmail.com>
2025-01-17 15:09:19 -0300

    chore: reorganize inputs and assets

---
+++ src/app/components/forms/forms.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forms',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.scss'
+})
+export class FormsComponent {
+
+}
